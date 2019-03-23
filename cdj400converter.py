53a7185c830b4e4fd7645015605db2cc4b41e046
André "MG" Wisén <andre.wisen@gmail.com>
2019-03-23 13:57:45 +0100

    Update main function. Script now ignores all suffix

--- cdj400converter.py
+++ cdj400converter.py
@@ -2,28 +2,28 @@
 # -*- coding: utf-8 -*-
 # Written by: Andre MG Wisen
 # https://andrewisen.se/
-
 import os
-from fnmatch import fnmatch
-
 
 def main():
 	# === SETTINGS ===
 	delimiter = "."
 	filenameSuffix = "mp3"
- 	
-	currentDirectory= os.getcwd()
+	currentDirectory = os.getcwd()
 
 	try:
 		for root, directories, files in os.walk(currentDirectory):
 				for file in files:
-					if (delimiter + filenameSuffix) not in file:
-						oldFile = os.path.join(root, file)
-						newFile = oldFile + delimiter + filenameSuffix
-						os.rename(oldFile, newFile)
+					try:
+						filename, suffix = file.split(delimiter)
+						continue
+					except:
+						pass
+					oldFile = os.path.join(root, file)
+					newFile = oldFile + delimiter + filenameSuffix
+					print(oldFile,newFile)
+					os.rename(oldFile, newFile)
 	except Exception as e:
 		print(e)
 	return
-
 if __name__== "__main__":
 	main()
